a6c9f9b8c39e9a67e8d220dbbdc5005ed48dea7d
TranexTrane <denoncinedouard@hotmail.be>
2021-01-28 10:43:34 +0100

    push du truc pas ouf

---
+++ js/main.js
@@ -0,0 +1,5 @@
+let off = document.getElementsByClassName('.revealable');
+let on = document.getElementsByClassName('.revealed');
+let img = document.getElementsByClassName('img-fluid');
+let height = img.clientHeight;
+
